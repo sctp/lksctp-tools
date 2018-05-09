03889e53e6c09f7c9b58c4ffa236b62a0d19793b
Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
2018-05-09 00:51:23 -0300

    travis: integrate with Coverity Scan
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>

--- .travis/linux-build.sh
+++ .travis/linux-build.sh
@@ -15,7 +15,12 @@ function cleanup()
 
 function clone_kernel()
 {
-	git clone https://github.com/torvalds/linux
+	if ! [[ $KERNEL =~ \\s ]]; then
+		depth="--depth=50"
+	else
+		depth=""
+	fi
+	git clone $depth https://github.com/torvalds/linux
 }
 
 function download_kernel()
@@ -84,6 +89,12 @@ function build_lksctp()
 	fi
 }
 
+# When building the coverity_scan branch, allow only the 6th job to continue
+# to avoid travis-ci/travis-ci#1975. 6th = gcc7, linux tip
+if [[ "${TRAVIS_BRANCH}" == "coverity_scan" ]]; then
+	exit 0
+fi
+
 trap cleanup EXIT
 if [ -z "$KERNEL" ]; then
 	clone_kernel
@@ -94,8 +105,13 @@ if [ -z "$KERNEL" ]; then
 		build_lksctp 1
 	done
 else
-	download_kernel "$KERNEL"
-	download_prep_kernel "$KERNEL"
+	if [ "$KERNEL" == 'master' ]; then
+		clone_kernel
+		git_prep_kernel "$KERNEL"
+	else
+		download_kernel "$KERNEL"
+		download_prep_kernel "$KERNEL"
+	fi
 	build_lksctp
 	build_lksctp 1
 fi
