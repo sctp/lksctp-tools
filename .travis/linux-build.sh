ecd8fb79d59c77e8d9fbadbbfac0ac463799334d
Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
2018-08-01 13:33:05 -0300

    travis: use git instead of downloading tarballs
    With this, we 1) avoid downloading several tarballs and 2) make it
    easier to test against Linus master.
    
    We want to test against Linus master so can catch build errors before
    GA, like the one fixed by Xin's patches in the sequence.
    
    Sample build: https://travis-ci.org/sctp/lksctp-tools/builds/410860154
    
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>

--- .travis/linux-build.sh
+++ .travis/linux-build.sh
@@ -1,17 +1,34 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -ex
+VERS="master v4.17 v4.16 v4.13 v4.12 v4.11 v4.10"
 
 nproc=$(/usr/bin/getconf _NPROCESSORS_ONLN)
+basedir=$(pwd)
 
-function install_kernel()
+function cleanup()
+{
+	cd "$basedir"
+	[ ! -d linux ] || rm -rf linux
+	[ ! -d "linux-$KERNEL" ] || rm -rf "linux-$KERNEL"
+	make distclean || :
+}
+
+function clone_kernel()
+{
+	git clone https://github.com/torvalds/linux
+}
+
+function download_kernel()
 {
 	VER="$1"
 	URL="https://www.kernel.org/pub/linux/kernel/v4.x/linux-$VER.tar.xz"
 	wget "$URL"
 	tar xf "linux-$VER.tar.xz"
+}
 
-	pushd "linux-$VER"
+function __prep_kernel()
+{
+	make mrproper
 	make allmodconfig
 	make -j $nproc modules_prepare
 	make -j $nproc headers_install
@@ -19,8 +36,26 @@ function install_kernel()
 	popd
 }
 
+function git_prep_kernel()
+{
+	VER="$1"
+
+	pushd "linux"
+	git checkout "$VER"
+	__prep_kernel
+}
+
+function download_prep_kernel()
+{
+	VER="$1"
+
+	pushd "linux-$VER"
+	__prep_kernel
+}
+
 function build_lksctp()
 {
+	make distclean || :
 	./bootstrap
 
 	#CFLAGS="-Werror"
@@ -33,11 +68,19 @@ function build_lksctp()
 	make -j $nproc
 
 	#make -j $nproc distcheck
+
 }
 
-if [ -n "$KERNEL" ]; then
-	install_kernel "$KERNEL"
+trap cleanup EXIT
+if [ -z "$KERNEL" ]; then
+	clone_kernel
+
+	for ver in $VERS; do
+		git_prep_kernel "$ver"
+		build_lksctp
+	done
+else
+	download_kernel "$KERNEL"
+	download_prep_kernel "$KERNEL"
+	build_lksctp
 fi
-
-build_lksctp
-
