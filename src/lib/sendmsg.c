sctp/lksctp-tools

commit 91239acfc91f0bbd9e28ba84049a40c747c391db

Add sctp_send() API support and testcases

src/lib/sendmsg.c
@@ -66,3 +66,41 @@ sctp_sendmsg(int s, const void *msg, size_t len, struct sockaddr *to,
 
 	return sendmsg(s, &outmsg, 0);
 }
+
+/* This library function assist the user with sending a message without
+ * dealing directly with the CMSG header.
+ */
+int
+sctp_send(int s, const void *msg, size_t len,
+          const struct sctp_sndrcvinfo *sinfo, int flags)
+{
+	struct msghdr outmsg;
+	struct iovec iov;
+
+	outmsg.msg_name = NULL;
+	outmsg.msg_namelen = 0;
+	outmsg.msg_iov = &iov;
+	iov.iov_base = (void *)msg;
+	iov.iov_len = len;
+	outmsg.msg_iovlen = 1;
+	outmsg.msg_controllen = 0;
+
+	if (sinfo) {	
+		char outcmsg[CMSG_SPACE(sizeof(struct sctp_sndrcvinfo))];
+		struct cmsghdr *cmsg;
+
+		outmsg.msg_control = outcmsg;
+		outmsg.msg_controllen = sizeof(outcmsg);
+		outmsg.msg_flags = 0;
+
+		cmsg = CMSG_FIRSTHDR(&outmsg);
+		cmsg->cmsg_level = IPPROTO_SCTP;
+		cmsg->cmsg_type = SCTP_SNDRCV;
+		cmsg->cmsg_len = CMSG_LEN(sizeof(struct sctp_sndrcvinfo));
+
+		outmsg.msg_controllen = cmsg->cmsg_len;
+		memcpy(CMSG_DATA(cmsg), sinfo, sizeof(struct sctp_sndrcvinfo));
+	}
+
+	return sendmsg(s, &outmsg, flags);
+}

src/func_tests/test_1_to_1_send.c
@@ -58,7 +58,7 @@
 #include <sctputil.h>
 
 char *TCID = __FILE__;
-int TST_TOTAL = 8;
+int TST_TOTAL = 9;
 int TST_CNT = 0;
 
 int
@@ -215,6 +215,11 @@ main(int argc, char *argv[])
 	test_recv(acpt_sk, message_rcv, 5, flag);
 	
 	tst_resm(TPASS, "send() partial data from a buffer - SUCCESS");
+
+	/* TEST9: sctp_send with no sinfo */
+	test_sctp_send(sk, message, strlen(message) + 1 , NULL, flag);
+	test_recv(acpt_sk, message_rcv, strlen(message) + 1, flag);
+	tst_resm(TPASS, "sctp_send() with no sinfo - SUCCESS");
 
 	close(sk1);
 	close(lstn_sk);

src/func_tests/test_sctp_sendrecvmsg.c
@@ -56,7 +56,7 @@
 #include <sctputil.h>
 
 char *TCID = __FILE__;
-int TST_TOTAL = 9;
+int TST_TOTAL = 10;
 int TST_CNT = 0;
 
 #define SMALL_RCVBUF 800
@@ -84,6 +84,8 @@ int main(int argc, char *argv[])
 	size_t buflen;
 	struct sctp_send_failed *ssf;
 	struct sctp_sndrcvinfo sinfo;
+	struct sctp_sndrcvinfo snd_sinfo;
+	sctp_assoc_t associd1, associd2;
 
 	/* Rather than fflush() throughout the code, set stdout to
 	 * be unbuffered.
@@ -166,6 +168,7 @@ int main(int argc, char *argv[])
 	error = test_sctp_recvmsg(sk2, big_buffer, buflen,
 				  (struct sockaddr *)&msgname, &msgname_len,
 				  &sinfo, &msg_flags); 
+	associd2 = ((struct sctp_assoc_change *)big_buffer)->sac_assoc_id;
 	test_check_buf_notification(big_buffer, error, msg_flags,
 				    sizeof(struct sctp_assoc_change),
 				    SCTP_ASSOC_CHANGE, SCTP_COMM_UP);
@@ -176,6 +179,7 @@ int main(int argc, char *argv[])
 	error = test_sctp_recvmsg(sk1, big_buffer, buflen,
 				  (struct sockaddr *)&msgname, &msgname_len,
 				  &sinfo, &msg_flags); 
+	associd1 = ((struct sctp_assoc_change *)big_buffer)->sac_assoc_id;
 	test_check_buf_notification(big_buffer, error, msg_flags,
 				    sizeof(struct sctp_assoc_change),
 				    SCTP_ASSOC_CHANGE, SCTP_COMM_UP);
@@ -293,6 +297,25 @@ int main(int argc, char *argv[])
 
 	tst_resm(TPASS, "sctp_recvmsg SEND_FAILED for fragmented message with "
 		 "ttl");
+
+	snd_sinfo.sinfo_ppid = rand();
+	snd_sinfo.sinfo_flags = 0; 
+	snd_sinfo.sinfo_stream = 2; 
+	snd_sinfo.sinfo_timetolive = 0; 
+	snd_sinfo.sinfo_assoc_id = associd1; 
+	test_sctp_send(sk1, message, strlen(message) + 1, &snd_sinfo,
+		       MSG_NOSIGNAL);
+
+	buflen = REALLY_BIG;
+	msgname_len = sizeof(msgname);
+	error = test_sctp_recvmsg(sk2, big_buffer, buflen,
+				  (struct sockaddr *)&msgname, &msgname_len,
+				  &sinfo, &msg_flags); 
+	test_check_buf_data(big_buffer, error, msg_flags, &sinfo,
+			    strlen(message) + 1, MSG_EOR, snd_sinfo.sinfo_stream,
+			    snd_sinfo.sinfo_ppid); 
+
+	tst_resm(TPASS, "sctp_send");
 
 	/* Shut down the link.  */
 	close(sk1);

src/include/netinet/sctp.h
@@ -654,6 +654,12 @@ int sctp_sendmsg(int s, const void *msg, size_t len, struct sockaddr *to,
 		 socklen_t tolen, uint32_t ppid, uint32_t flags,
 		 uint16_t stream_no, uint32_t timetolive, uint32_t context);
 
+/* This library function assist the user with sending a message without
+ * dealing directly with the CMSG header.
+ */
+int sctp_send(int s, const void *msg, size_t len,
+              const struct sctp_sndrcvinfo *sinfo, int flags);
+
 /* This library function assists the user with the advanced features
  * of SCTP.  This is a new SCTP API described in the section 8.8 of the
  * Sockets API Extensions for SCTP. This is implemented using the

src/testlib/sctputil.h
@@ -281,6 +281,17 @@ static inline int test_sctp_sendmsg(int s, const void *msg, size_t len,
 	return error;			
 }
 
+static inline int test_sctp_send(int s, const void *msg, size_t len,
+				 const struct sctp_sndrcvinfo *sinfo, 
+				 int flags)
+{
+	int error = sctp_send(s, msg, len, sinfo, flags);
+	if (len != error)
+		tst_brkm(TBROK, tst_exit, "sctp_send: error:%d errno:%d",
+			 error, errno);
+	return error;			
+}
+
 static inline int test_sctp_recvmsg(int sk, void *msg, size_t len,
 				    struct sockaddr *from, socklen_t *fromlen,
 				    struct sctp_sndrcvinfo *sinfo,

commit e2cefbe1e6fba49c3077d0c68268db5d5a9a1da7

sctp_send: fix ignored flags parameter
CovScan noticed that outmsg.msg_flags was being used uninitialized
if sinfo was not provided. Lets fix it by initializing it with the flags
parameter, like sctp_sendv does.

Fixes: 91239acfc91f ("Add sctp_send() API support and testcases")
Reviewed-by: Xin Long <lucien.xin@gmail.com>
Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>

src/lib/sendmsg.c
@@ -77,25 +77,21 @@ int
 sctp_send(int s, const void *msg, size_t len,
           const struct sctp_sndrcvinfo *sinfo, int flags)
 {
-	struct msghdr outmsg;
+	struct msghdr outmsg = {};
 	struct iovec iov;
 	char outcmsg[CMSG_SPACE(sizeof(struct sctp_sndrcvinfo))];
 
-	outmsg.msg_name = NULL;
-	outmsg.msg_namelen = 0;
 	outmsg.msg_iov = &iov;
 	iov.iov_base = (void *)msg;
 	iov.iov_len = len;
 	outmsg.msg_iovlen = 1;
-	outmsg.msg_control = NULL;
-	outmsg.msg_controllen = 0;
+	outmsg.msg_flags = flags;
 
 	if (sinfo) {	
 		struct cmsghdr *cmsg;
 
 		outmsg.msg_control = outcmsg;
 		outmsg.msg_controllen = sizeof(outcmsg);
-		outmsg.msg_flags = 0;
 
 		cmsg = CMSG_FIRSTHDR(&outmsg);
 		cmsg->cmsg_level = IPPROTO_SCTP;